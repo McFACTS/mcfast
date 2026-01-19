790c5bef0310aebe6be10c23775b8f86e8ce2e55
nrposner <nicolasposner@gmail.com>
2026-01-19 16:08:40 -0500

    adding generate_r

--- src/accelerants/powerlaw.rs
+++ src/accelerants/powerlaw.rs
@@ -1,4 +1,6 @@
-use pyo3::prelude::*;
+use std::f64::consts::PI;
+
+use pyo3::{exceptions::PyValueError, prelude::*};
 use numpy::{PyArray1, PyArrayMethods, PyReadonlyArray1};
 use rayon::prelude::*;
 
@@ -69,6 +71,8 @@ pub fn dual_powerlaw<'py>(
     out_arr
 }
 
+// intended to be used as
+// r, y_unscaled = dual_powerlaw_with_grid(disk_inner_stable_circ_orb, disk_radius_outer, 1000000, nsc_radius_crit_rg, nsc_density_index_inner, nsc_density_index_outer)
 #[pyfunction]
 pub fn dual_powerlaw_with_grid<'py>(
     py: Python<'py>,
@@ -113,35 +117,75 @@ pub fn dual_powerlaw_with_grid<'py>(
     (r_arr, y_arr)
 }
 
+// to be used as r, r_pdf = generate_r(...)
+#[pyfunction]
+#[allow(clippy::type_complexity, clippy::too_many_arguments)]
+pub fn generate_r<'py>(
+    py: Python<'py>,
+    start: f64,
+    end: f64,
+    num_points: usize,
+    crit_radius: f64,
+    index_inner: f64,
+    index_outer: f64,
+    volume_scaling: bool,
+) -> PyResult<(Bound<'py, PyArray1<f64>>, Bound<'py, PyArray1<f64>>)> {
 
+    // given that y is just an intermediary, pay attention to whether or not we actually need to
+    // initialize separate vecs for it.
 
-// #[pyfunction]
-// pub fn dual_powerlaw<'py>(
-//     py: Python<'py>,
-//     radius: PyReadonlyArray1<'py, f64>,
-//     crit_radius: f64,
-//     index_inner: f64,
-//     index_outer: f64,
-// ) -> Bound<'py, PyArray1<f64>> {
-//     let radius_slice = radius.as_slice().unwrap();
-//
-//     // Create uninitialized array (faster than generic new which might zero-fill)
-//     let out_arr = unsafe { PyArray1::new(py, radius_slice.len(), false) };
-//     let out_slice = unsafe { out_arr.as_slice_mut().unwrap() };
-//
-//     // Pre-calculate negative indices to save 1 negation per loop
-//     let neg_inner = -index_inner;
-//     let neg_outer = -index_outer;
-//     // Pre-calculate inverse crit_radius to use multiplication (faster than division)
-//     let inv_crit = 1.0 / crit_radius;
-//
-//     for (i, r) in radius_slice.iter().enumerate() {
-//         if *r <= crit_radius {
-//             out_slice[i] = (r * inv_crit).powf(neg_inner);
-//         } else {
-//             out_slice[i] = (r * inv_crit).powf(neg_outer);
-//         }
-//     }
-//     out_arr
-// }
+    let r_arr = unsafe { PyArray1::new(py, num_points, false) };
+    // will be returned as_r_pdf at the end
+    let r_pdf_arr = unsafe { PyArray1::new(py, num_points, false) };
+    
+    let r_slice = unsafe { r_arr.as_slice_mut().unwrap() };
+    let y_slice = unsafe { r_pdf_arr.as_slice_mut().unwrap() };
+
+    // 2. Pre-calculate constants
+    // this is also the gradient from later
+    let step = (end - start) / ((num_points - 1) as f64);
+    let neg_inner = -index_inner;
+    let neg_outer = -index_outer;
+    let inv_crit = 1.0 / crit_radius;
+
+    // 3. Parallel fused generation
+    // We zip the two output slices together to write to them simultaneously
+    r_slice.par_iter_mut()
+        .zip(y_slice.par_iter_mut())
+        .enumerate()
+        .for_each(|(i, (r_out, y_out))| {
+            // Calculate grid point
+            let val = start + (i as f64 * step);
+            *r_out = val;
+
+            // Calculate powerlaw immediately (value is hot in register)
+            // the inner if/else check is always the same for each function call,
+            // so the branch predictor should be fine, but double check
+            if val <= crit_radius {
+                if volume_scaling {
+                    let shell_volume = PI * val.powi(2) * step;
+                    *y_out = (val * inv_crit).powf(neg_inner) * shell_volume;
+                } else {
+                    *y_out = (val * inv_crit).powf(neg_inner);
+                }
+            } else if volume_scaling {
+                let shell_volume = PI * val.powi(2) * step;
+                *y_out = (val * inv_crit).powf(neg_outer) * shell_volume;
+            } else {
+                *y_out = (val * inv_crit).powf(neg_outer);
+            }
+        });
+
+    // need to construct y first to get the sum and normalize, has to be a second pass
+    let y_sum: f64 = y_slice.par_iter().sum();
+    if y_sum == 0.0 {
+        return Err(PyValueError::new_err("[Setup BH Locs] sum(y) = 0. \nMust be non-zero for use as denominator during pdf normalization."))
+    }
+
+    // not clear that this second pass will also benefit from being parallelized, might or might
+    // not, try both
+    y_slice.par_iter_mut().for_each(|y| { *y /= y_sum; });
+
+    Ok((r_arr, r_pdf_arr))
+}
 
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ mod accelerants;
 mod constants;
 
 use accelerants::cubes::{encounters_new_orba_ecc, cubic_y_root_cardano, cubic_finite_step_root_cardano, transition_physical_as_el};
-use accelerants::powerlaw::{continuous_broken_powerlaw, dual_powerlaw, dual_powerlaw_with_grid};
+use accelerants::powerlaw::{continuous_broken_powerlaw, dual_powerlaw, dual_powerlaw_with_grid, generate_r};
 use accelerants::tau::{tau_ecc_dyn_helper, tau_inc_dyn_helper};
 use accelerants::kick::analytical_kick_velocity_helper;
 use tools::merge_tree::MergeForest;
@@ -21,6 +21,7 @@ fn mcfast(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(continuous_broken_powerlaw, m)?)?;
     m.add_function(wrap_pyfunction!(dual_powerlaw, m)?)?;
     m.add_function(wrap_pyfunction!(dual_powerlaw_with_grid, m)?)?;
+    m.add_function(wrap_pyfunction!(generate_r, m)?)?;
     m.add_function(wrap_pyfunction!(tau_ecc_dyn_helper, m)?)?;
     m.add_function(wrap_pyfunction!(tau_inc_dyn_helper, m)?)?;
     m.add_class::<MergeForest>()?;
